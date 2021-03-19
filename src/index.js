81067620ae69383383a7ffc6fee57f50b3c56ab9
Ndn1618 <ndilbar1618@gmail.com>
2021-03-19 20:19:49 +0500

    Getting started with Socket.io

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 const express = require('express')
+const http = require('http')
 const path = require('path')
+const socketIO = require('socket.io')
 
 const app = express()
+const server = http.createServer(app)
+const io = socketIO(server)
 
 const PORT = process.env.PORT || 3000
 const publicDirectoryPath = path.join(__dirname, '../public')
 
 app.use(express.static(publicDirectoryPath))
 
-app.listen(PORT, () => {
+io.on('connection', () => {
+  console.log('New connection')
+})
+
+server.listen(PORT, () => {
   console.log(`Server is up on port ${PORT}`)
 })
