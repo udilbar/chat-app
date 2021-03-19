3ae25056b190929d9b778f154b16693d8598c7fe
Ndn1618 <ndilbar1618@gmail.com>
2021-03-19 18:30:58 +0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+
+const PORT = process.env.PORT || 3000
+const publicDirectoryPath = path.join(__dirname, '../public')
+
+app.use(express.static(publicDirectoryPath))
+
+app.listen(PORT, () => {
+  console.log(`Server is up on port ${PORT}`)
+})
