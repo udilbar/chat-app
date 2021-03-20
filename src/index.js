dcf848e222d8d8c56b73e879c13c7a4387746405
Ndn1618 <ndilbar1618@gmail.com>
2021-03-20 09:20:23 +0500

    Event Acknowledgements

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const http = require('http')
 const path = require('path')
 const socketIO = require('socket.io')
+const Filter = require('bad-words')
 
 const app = express()
 const server = http.createServer(app)
@@ -19,12 +20,19 @@ io.on('connection', (socket) => {
 
   socket.broadcast.emit('message', 'A new user joined!')
 
-  socket.on('sendMessage', (message) => {
+  socket.on('sendMessage', (message, callback) => {
+    const filter = new Filter()
+    if (filter.isProfane(message)) {
+      return callback('Profanity is not allowed!')
+    }
+
     io.emit('message', message)
+    callback()
   })
 
-  socket.on('sendLocation', (coords) => {
+  socket.on('sendLocation', (coords, callback) => {
     io.emit('message', `https://google.com/maps?q=${coords.latitude},${coords.longitude}`)
+    callback()
   })
 
   socket.on('disconnect', () => {
