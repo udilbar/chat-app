535ec5791dc153f51ec07dc554c43d2eeffd4bba
Ndn1618 <ndilbar1618@gmail.com>
2021-03-20 18:22:44 +0500

    Storing users (part 1)

---
+++ src/utils/users.js
@@ -0,0 +1,47 @@
+const users = []
+
+const addUser = ({ id, username, room }) => {
+  // Clean the data
+  username = username.trim().toLowerCase()
+  room = room.trim().toLowerCase()
+
+  // Validate the data
+  if (!username || !room) {
+    return {
+      error: 'Username and room are required!'
+    }
+  }
+
+  // Check for existing user
+  const existingUser = users.find((user) => {
+    return user.room === room && user.username === username
+  })
+
+  // Validate username
+  if (existingUser) {
+    return {
+      error: 'Username is in use!'
+    }
+  }
+
+  // Store user
+  const user = { id, username, room }
+  users.push(user)
+  return { user }
+}
+
+const removeUser = (id) => {
+  const index = users.findIndex((user) => user.id === id)
+
+  if (index !== -1) {
+    return users.splice(index, 1)[0]
+  }
+}
+
+addUser({
+  id: 22,
+  username: 'Dilbar   ',
+  room: '    NDN'
+})
+
+console.log(removeUser(22))
